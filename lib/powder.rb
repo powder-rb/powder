804b78b844b73c2349a151e6df7f534a7d98c379
Haim Ashkenazi <haim@babysnakes.org>
2012-08-28 09:49:36 +0300

    Fixed 'powder list' to display proxy ports.

--- lib/powder.rb
+++ lib/powder.rb
@@ -1,2 +1,12 @@
 module Powder
+  # Get the origin of the application link, whether it's a link to a
+  # rack app or proxy to a port.
+  def get_app_origin(app_link)
+    if File.symlink? app_link
+      File.readlink(app_link)
+    else
+      port = File.readlines(app_link)[0]
+      "proxy port: #{port}"
+    end
+  end
 end
