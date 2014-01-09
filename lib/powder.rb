86c6dfcb623b91021b6798f67aa6da38e292c9f2
Donald Guy <fawkes@mit.edu>
2014-01-08 21:34:42 -0500

    list: handle non-symlink dirs and URLs in POW_PATH
    Changes helper method get_app_origin to conform much closer to the logic
    used by pow itself.
    
    In the process, introduces reporting-support for URL proxy files,
    removes crashes on encountering non-symlink dirs, and ignores extraneous
    files (e.g. badly formatted proxying files or, if they somehow appear, unix
    sockets, device files, etc.)

--- lib/powder.rb
+++ lib/powder.rb
@@ -1,12 +1,30 @@
 module Powder
   # Get the origin of the application link, whether it's a link to a
-  # rack app or proxy to a port.
+  # rack app, proxy to a port, or proxy to a URL.
+  #
+  # Mostly follows logic of pow configuration class last changed 2011-08-05,
+  # which remains current as of pow v0.4.1 (and master on 2014-01-08)
+  # http://github.com/37signals/pow/blob/36b91fe/lib/configuration.js#L161-L188
   def get_app_origin(app_link)
-    if File.symlink? app_link
-      File.readlink(app_link)
+    case (File.stat(app_link).ftype)
+    when "file"
+      data = File.read(app_link)
+      if data.length < 10 && data.to_i != 0
+        "proxy port: #{data}"
+      elsif url_match = (data.match %r{https?://\S+})
+        "proxy URL: #{url_match.to_s}"
+      else
+        nil
+      end
+    when "directory"
+      canonicalize_path(app_link)
     else
-      port = File.readlines(app_link)[0]
-      "proxy port: #{port}"
+      nil
     end
+  end
+
+  def canonicalize_path(path)
+      path = File.readlink path if File.symlink? path
+      path.sub(ENV['HOME'], '~')
   end
 end
