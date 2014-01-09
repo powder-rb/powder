001b905b257385f64e7d58507250761d6efa19de
Donald Guy <fawkes@mit.edu>
2014-01-08 21:43:32 -0500

    list: report on symlinked public dirs, Fixes #84

--- lib/powder.rb
+++ lib/powder.rb
@@ -5,6 +5,10 @@ module Powder
   # Mostly follows logic of pow configuration class last changed 2011-08-05,
   # which remains current as of pow v0.4.1 (and master on 2014-01-08)
   # http://github.com/37signals/pow/blob/36b91fe/lib/configuration.js#L161-L188
+  #
+  # Also indicates origin of ./public if a symlink, as requested in issue #84
+  # (perhaps of use when serving static files as described in
+  # http://pow.cx/manual.html#section_2.4 )
   def get_app_origin(app_link)
     case (File.stat(app_link).ftype)
     when "file"
@@ -17,7 +21,11 @@ module Powder
         nil
       end
     when "directory"
-      canonicalize_path(app_link)
+      app_pubdir = app_link + File::Separator + "public"
+      public_note = (File.symlink? app_pubdir ?
+                       " (with /public -> #{canonicalize_path(app_pubdir)})"
+                     : "" )
+      canonicalize_path(app_link) + public_note
     else
       nil
     end
